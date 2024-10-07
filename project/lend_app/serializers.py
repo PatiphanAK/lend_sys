ae66c03bf62fb906c3b47e9b4b5ae342af6fe1e6
PatiphanAK <107915386+PatiphanAK@users.noreply.github.com>
2024-10-08 01:06:05 +0700

    update serializer Borrower

--- project/lend_app/serializers.py
+++ project/lend_app/serializers.py
@@ -3,6 +3,8 @@ from .models import Organization, Category, Borrower, Approver, Item, EquipmentS
 from django.utils import timezone
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.hashers import make_password
+from rest_framework.exceptions import ValidationError
+
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,15 +22,26 @@ class BorrowerSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', write_only=True)
     email = serializers.EmailField(source='user.email', write_only=True)
     password = serializers.CharField(write_only=True)
+    profile_image = serializers.URLField(required=False, allow_null=True)
+    fname = serializers.CharField(
+        source='user.first_name', write_only=True)  # เพิ่มฟิลด์ fname
+    lname = serializers.CharField(
+        source='user.last_name', write_only=True)   # เพิ่มฟิลด์ lname
 
     class Meta:
         model = Borrower
-        fields = ['id', 'profile_image', 'description', 'username', 'email', 'password']
+        fields = ['id', 'profile_image', 'description',
+                  'username', 'email', 'password', 'fname', 'lname']
 
     def create(self, validated_data):
-        # Get user data
         user_data = validated_data.pop('user')
+        username = user_data.get('username')
         password = user_data.pop('password')
+
+        # Check if username already exists
+        if User.objects.filter(username=username).exists():
+            raise ValidationError(
+                {"username": "This username is already taken."})
 
         # Create user and set password
         user = User(**user_data)
@@ -50,6 +63,7 @@ class ApproverSerializer(serializers.ModelSerializer):
         model = Approver
         fields = '__all__'
 
+
 class ApproverSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', write_only=True)
     email = serializers.EmailField(source='user.email', write_only=True)
@@ -57,7 +71,8 @@ class ApproverSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Approver
-        fields = ['id', 'profile_image', 'description', 'username', 'email', 'password']
+        fields = ['id', 'profile_image', 'description',
+                  'username', 'email', 'password']
 
     def create(self, validated_data):
         # Get user data
@@ -77,6 +92,7 @@ class ApproverSerializer(serializers.ModelSerializer):
         user.groups.add(approver_group)
 
         return approver
+
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -122,7 +138,8 @@ class BorrowRequestSerializer(serializers.ModelSerializer):
             )
 
         return attrs
-   
+
+
 class BorrowQueueSerializer(serializers.ModelSerializer):
     class Meta:
         model = BorrowQueue
--- project/lend_app/urls.py
+++ project/lend_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BorrowerView
+from .views import BorrowerView, BorrowerRegisterView #Borrower
 urlpatterns = [
     path('borrowers/', BorrowerView.as_view(), name='borrower-list'),
+    path('borrowers/register/', BorrowerRegisterView.as_view(), name='borrower-register'),
 ]
